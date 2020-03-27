d45f1522e89a1a0886caa490254236bf0945abd0
Geethkiran <61141781+Geethkiran@users.noreply.github.com>
2020-03-27 15:44:30 +0530

    mood analyser exception class

---
+++ MoodAnalyserException.java
@@ -0,0 +1,17 @@
+package com.MoodAnalyser.java;
+
+public class MoodAnalyserException extends Throwable {
+    public enum ExceptionType { ENTERED_NULL, ENTERED_EMPTY, NO_SUCH_CLASS, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE,
+        NO_ACCESS, OBJECT_CREATION_ISSUE,NO_SUCH_FIELD } ;
+
+    public ExceptionType type;
+
+    public MoodAnalyserException(String message) {
+        super(message);
+    }
+
+    public MoodAnalyserException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+}
